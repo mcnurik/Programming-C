03c51fc878db143ad6a474472d6d8cf46c9ccb6c
mcnurik <nuriddin199624@gmail.com>
2021-03-02 23:19:45 +0500

    Изменил y на z

--- Work -1/Task-1-1.cpp
+++ Work -1/Task-1-1.cpp
@@ -10,7 +10,7 @@ int main()
 	const double y = 17.5;
 	const double z = 3.2;
 
-	const auto a = (y*z*x*x) - z / sin(2 * (x / y));
+	const auto a = (y*z*x*x) - z / (sin*(x / z)*sin*(x/z);
 	const auto b = z * exp(-sqrt(z))*(cos(y*x / z)*cos(y*x / z));
 
 	cout << a << ' ' << b;
